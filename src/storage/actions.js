d71ac5d2e7d5e9ce2c740f867f273dc23cb61797
Marcelo Alaniz <alanizmarcelo@gmail.com>
2021-02-25 13:47:00 -0300

    refact: actions

--- src/storage/actions.js
+++ src/storage/actions.js
@@ -1,38 +1,38 @@
 export const actionTypes = {
-    UPDATE_PAGE: "UPDATE_PAGE",
-    UPDATE_OPTION: "UPDATE_OPTION",
-    UPDATE_NAME: "UPDATE_NAME",
-    UPDATE_EMAIL: "UPDATE_EMAIL",
-    UPDATE_PHONE: "UPDATE_PHONE",
-    UPDATE_BUDGET: "UPDATE_BUDGET",
+  UPDATE_PAGE: "UPDATE_PAGE",
+  UPDATE_OPTION: "UPDATE_OPTION",
+  UPDATE_NAME: "UPDATE_NAME",
+  UPDATE_EMAIL: "UPDATE_EMAIL",
+  UPDATE_PHONE: "UPDATE_PHONE",
+  UPDATE_BUDGET: "UPDATE_BUDGET",
 }
 
 export const updatePage = data => ({
-    type: actionTypes.UPDATE_PAGE,
-    data
-});
+  type: actionTypes.UPDATE_PAGE,
+  data
+})
 
 export const updateOption = data => ({
-    type: actionTypes.UPDATE_OPTION,
-    data
-});
+  type: actionTypes.UPDATE_OPTION,
+  data
+})
 
 export const updateName = data => ({
-    type: actionTypes.UPDATE_NAME,
-    data
-});
+  type: actionTypes.UPDATE_NAME,
+  data
+})
 
 export const updateEmail = data => ({
-    type: actionTypes.UPDATE_EMAIL,
-    data
-});
+  type: actionTypes.UPDATE_EMAIL,
+  data
+})
 
 export const updatePhone = data => ({
-    type: actionTypes.UPDATE_PHONE,
-    data
-});
+  type: actionTypes.UPDATE_PHONE,
+  data
+})
 
 export const updateBudget = data => ({
-    type: actionTypes.UPDATE_BUDGET,
-    data
-});
+  type: actionTypes.UPDATE_BUDGET,
+  data
+})
